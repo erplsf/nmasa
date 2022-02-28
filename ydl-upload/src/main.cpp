10384360a95ed0a6eaa6dacb13bf97cdd3b96e6c
Andriy Mykhaylyk <andriy.mykhaylyk@klar.mx>
2022-02-28 14:40:45 +0100

    ugly but works

--- ydl-upload/src/main.cpp
+++ ydl-upload/src/main.cpp
@@ -1,6 +1,28 @@
 #include <fmt/core.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[]) {
-  fmt::print("got {} args.\n", argc);
+const size_t chunk_size = 1024;
+
+int main(int argc, char* argv[]) {
   (void)argv;
+  if (argc != 2) {
+    fmt::print(stderr, "Exactly one argument is expected - video URL!\n");
+    return EXIT_FAILURE;
+  }
+  const auto url = argv[1];
+  // fmt::print("url: {}\n", url);
+  const auto cmd = fmt::format("yt-dlp -j '{}'", url);
+  const auto pipe = popen(cmd.c_str(), "r");
+  std::string result;
+  std::string buf;
+  buf.resize(chunk_size);
+  size_t bytes;
+  while ((bytes = fread(buf.data(), 1, buf.size(), pipe)) > 0) {
+    fmt::print(stderr, "chars read: {}\n", bytes);
+    result.append(buf.begin(), buf.begin() + bytes);
+  };
+  // fmt::print("read: {}", result);
+  fmt::print("{}", result);
+  pclose(pipe);
+  return EXIT_SUCCESS;
 }
